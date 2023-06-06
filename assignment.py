48ffe4156e45e5b1725b90189b313159baf0fb1d
zinosalaam1 <bolatitosalaam7@gmail.com>
2023-06-05 23:42:55 -1200

    assignment

---
+++ assignment.py
@@ -0,0 +1,106 @@
+﻿'''
+You recently gotnabgot a job offer as a junior database administrator at a local restaurant chain. 
+You are to update the restaurant database to facilitate business operations. 
+
+Estimated completion time: 45mins
+Author: Olotu Praise Jah
+'''
+
+'''
+TASK 1: Create a new database named 'restaurant'
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 2: Verify that your database is created.
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 3: Create a new table named 'bookings', and verify it is created.
+The table has the following columns: id (primary key)(int), firstname(varchar), secondname(varchar), email(varchar), reservationno(varchar)
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 5: Add the following into the table
+'''
+'''
+'Tomiwa', 'Adeyanju', 'tom@mail.com', 'Table 1', 
+'Precious', 'Garba', 'presh@mail.com', 'Table 11',
+'Daniel', 'Akinje', 'dant@mail.com', 'Table 3',
+'Tomiwa', 'Lawal', 'md@mail.com', 'Table 15',
+'Lawal', 'Yusuf', 'lawal15@mail.com', 'Table 3',
+'Olamide', 'Opeyemi', 'Horpi@mail.com', 'Table 2',
+'Victor', 'Gabriel', 'gab@mail.com', 'Table 2',
+'Mayowa', 'Fatai', 'mayo@mail.com', 'Table 2',
+'Daniel', 'Lawal', 'xyz@mail.com', 'Table 1',
+'Daniel', 'Bright', 'daniboy@mail.com', 'Table 1',
+'Dorime', 'Akpan', 'pinkyqueen@mail.com', 'Table 2',
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 6: A customer called to check if his data was captured in the database, he said his last name is 'Akinje', write a SQL query to find out if his name is truly captured. 
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 7: Write a SQL query to find out how many occurrence of 'Daniel' is present as the first name. Make sure your query prevents SQL injection.
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 8: Write out a SQL query to find out the most booked table. Make sure your query prevents sqlSQL injection
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 9: Write out a SQL query to find out the names of those who booked Table 2. Make sure your query prevents SQL injection.
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 10: Write a SQL query that orders the first name in alphabetical order. 
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 11: A customer whose email address is gab@mail.com wants to change his reservation number to Table 5. Write a SQL query to update your record. Make sure you prevent SQL injection
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 12: Your line manager called you to notify you that the company is willing to give a 5% discount to the first 5 customers who reserved a sit at the restaurant. Write a SQL query to find out who this customers are.
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 13: After some while, your line manager called again to notify you that the fourth and fifth customer will instead receive a 3.5% discount. Write a SQL query to find out who they are.
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 14: Unfortunately a customer with an email address of pinkyqueen@mail.com called to notify of her reservation cancellation, you are to delete her record from the database.
+'''
+# your code here
+# don't forget to setup your database configuration
+
+'''
+Task 15: Your line manager asked you to create a new table named 'prevcustomer', with columns of firstname, email, reservationno. Create this table. 
+After some while, your line manager called to notify you to delete that table as it doesn't impact business operations. 
+'''
+# your code here
+# don't forget to setup your database configuration
---
+++ solution.py
@@ -0,0 +1,93 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Serifat2$",
+  database = 'restaurant'
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE restaurant")
+
+# mycursor.execute("SHOW DATABASES")
+
+# for x in mycursor:
+#     print(x)
+
+# mycursor.execute("CREATE TABLE bookings (id INT AUTO_INCREMENT PRIMARY KEY , firstname VARCHAR(255), secondname VARCHAR(255), email VARCHAR(255), reservationno VARCHAR(255))")
+
+# sql = "INSERT INTO bookings (firstname, secondname, email, reservationno) VALUES (%s, %s, %s, %s)"
+# val = [
+#     ("Tomiwa", "Adeyanju", "tom@mail.com", "Table 1"), 
+#     ("Precious", "Garba", "presh@mail.com", "Table 11"),
+#     ("Daniel", "Akinje", "dant@mail.com", "Table 3"),
+#     ("Tomiwa", "Lawal", "md@mail.com", "Table 15"),
+#     ("Lawal", "Yusuf", "lawal15@mail.com", "Table 3"),
+#     ("Olamide", "Opeyemi", "Horpi@mail.com", "Table 2"),
+#     ("Victor", "Gabriel", "gab@mail.com", "Table 2"),
+#     ("Mayowa", "Fatai", "mayo@mail.com", "Table 2"),
+#     ("Daniel", "Lawal", "xyz@mail.com", "Table 1"),
+#     ("Daniel", "Bright", "daniboy@mail.com", "Table 1"),
+#     ("Dorime", "Akpan", "pinkyqueen@mail.com", "Table 2"),
+# ]
+
+# mycursor.executemany(sql, val)
+
+# mydb.commit()
+
+# sql = "SELECT * FROM bookings WHERE secondname LIKE '%akinje%' "
+# mycursor.execute(sql)
+
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#     print(x)
+
+# sql = "SELECT * FROM bookings WHERE firstname = %s "
+# val = ("Daniel",)
+# mycursor.execute(sql, val)
+
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#     print(x)
+
+# sql = "SELECT * FROM bookings WHERE reservationno = %s "
+# val = ("Table 2",)
+# mycursor.execute(sql, val)
+
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#     print(x)
+
+# sql = "SELECT * FROM bookings ORDER BY firstname ASC "
+
+# mycursor.execute(sql)
+
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#     print(x)
+
+# sql = "UPDATE bookings SET email = %s WHERE email = %ab"
+
+# val = ("Table 5","Table 2")
+
+# mycursor.execute(sql, val)
+
+# mydb.commit()
+
+# mycursor.execute("SELECT * FROM bookings")
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#     print(x)
+
+mycursor.execute("SELECT * FROM bookings LIMIT 2 OFFSET 7")
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
